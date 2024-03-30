658df9ea0e9e52d56ea7023501819fb141da9bea
Rahul5045 <165356995+Rahul5045@users.noreply.github.com>
2024-03-30 01:33:36 -0700

    Delete login directory

--- login/login.js
+++
@@ -1,7 +0,0 @@
-let login=document.getElementById("login");
-let container=document.getElementById("div");
-
-login.addEventListener("click",()=>{
-    // window.alert("clicked");
-    container.style.visibility="hidden";
-})
