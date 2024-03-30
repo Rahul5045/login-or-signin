301ccc3ce4e1a2cbaf05c4a5424fce34069eaf88
Rahul5045 <165356995+Rahul5045@users.noreply.github.com>
2024-03-30 04:31:01 -0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,7 @@
+let login=document.getElementById("sign");
+let container=document.getElementById("div");
+
+login.addEventListener("click",()=>{
+    // window.alert("clicked");
+    container.style.visibility="hidden";
+})
