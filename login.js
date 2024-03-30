833af270ed89af3fdf28fa02a337bb0e2efa1a0e
Rahul5045 <165356995+Rahul5045@users.noreply.github.com>
2024-03-30 01:34:21 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+let login=document.getElementById("login");
+let container=document.getElementById("div");
+
+login.addEventListener("click",()=>{
+    // window.alert("clicked");
+    container.style.visibility="hidden";
+})
